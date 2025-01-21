b52b79cdc6bcf7e4d38291273c956fe4874d6f8b
Tina Huynh <tinamchuynh@gmail.com>
2025-01-20 21:32:31 -0800

    create about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,198 @@
+import { SidebarSeparator } from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+
 const aboutUs = () => {
   return (
-    <main>
-      <h1>About Us</h1>
-      <p>This is the about us page.</p>
+    <main className="w-11/12 mx-auto py-3">
+      <div>
+        <h1>About Us</h1>
+        <p>
+          At Crimson Lotus Creations, we blend cultural elegance with
+          cutting-edge technology to create bespoke digital experiences.
+          Inspired by the lotus flower—a symbol of growth, resilience, and
+          beauty—we specialize in helping small businesses flourish in the
+          digital world.
+        </p>
+        <p>
+          We offer tailored web development services, including custom website
+          creation, brand reimagining, and website makeovers, all designed to
+          bring your vision to life. Our team is dedicated to crafting modern,
+          approachable designs that reflect your unique brand identity and
+          resonate with your audience.
+        </p>
+        <p>
+          Rooted in the values of excellence, creativity, and authenticity,
+          Crimson Lotus Creations is here to empower your business with a
+          digital presence that feels personal, professional, and inspiring.
+          Let’s work together to transform your online presence and help your
+          business bloom.
+        </p>
+      </div>
+      <SidebarSeparator className="bg-secondary" />
+      <div className="py-3">
+        <h2>Mission Statement</h2>
+        <p>
+          At Crimson Lotus Creations, our mission is to empower small businesses
+          by transforming their digital presence into a powerful reflection of
+          their brand identity. Rooted in the values of creativity,
+          authenticity, and cultural elegance, we craft modern and approachable
+          web solutions that inspire growth, foster connections, and help
+          businesses thrive in the digital age. Through innovation and
+          collaboration, we aim to bring visions to life and create lasting
+          impacts for our clients and their communities.
+        </p>
+        <p>
+          Our mission is to create innovative, bespoke digital experiences that
+          inspire, empower, and empathize with our clients. We strive to deliver
+          a world class product, with a focus on creating experiences that are
+          both user-friendly and valuable.
+        </p>
+        <p>
+          We believe that our success is not just about delivering a product,
+          but about creating a community that values our clients' experiences
+          and supports them in their journey towards success. We are committed
+          to building a team of experts, passionate, and dedicated to delivering
+          exceptional digital experiences for our clients.
+        </p>
+      </div>
+      <SidebarSeparator className="bg-secondary" />
+      <div className="py-3">
+        <h2>Vision Statement</h2>
+        <p>
+          At Crimson Lotus Creations, our vision is to be the premier provider
+          of bespoke digital experiences that empower small businesses to create
+          lasting, meaningful, and impactful digital presence. We believe that
+          our success is not just about delivering a product, but about creating
+          a community that values our clients' experiences and supports them in
+          their journey towards success. We are committed to building a team of
+          experts, passionate, and dedicated to delivering exceptional digital
+          experiences for our clients.
+        </p>
+        <p>
+          At Crimson Lotus Creations, we envision a world where every small
+          business has the opportunity to shine in the digital space. By
+          blending cultural elegance with cutting-edge technology, we strive to
+          set a new standard for web development—creating meaningful,
+          user-focused designs that empower businesses to connect, grow, and
+          succeed. Our goal is to be a trusted partner in building a more
+          innovative, inclusive, and inspired digital future for businesses
+          everywhere.
+        </p>
+        <p>
+          Our team is committed to delivering exceptional digital experiences
+          that inspire, empower, and empathize with our clients. We are
+          dedicated to building a team of experts, passionate, and dedicated to
+          delivering exceptional digital experiences for our clients.
+        </p>
+      </div>
+      <SidebarSeparator className="bg-secondary" />
+      <div className="py-3">
+        <h2>Core Values</h2>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Creativity and Innovation:
+            </span>{" "}
+            We embrace creativity and leverage cutting-edge technology to
+            deliver unique, impactful digital solutions that bring our clients’
+            visions to life.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Authenticity:
+            </span>{" "}
+            We prioritize authenticity by creating digital experiences that
+            reflect the authenticity of our clients and their brand identity.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Cultural Elegance:
+            </span>{" "}
+            We believe that our success is not just about delivering a product,
+            but about creating a community that values our clients' experiences
+            and supports them in their journey towards success.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Excellence:
+            </span>{" "}
+            We strive to deliver exceptional digital experiences that inspire,
+            empower, and empathize with our clients.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Empowerment:
+            </span>{" "}
+            We are dedicated to empowering small businesses with the tools,
+            knowledge, and support they need to thrive in the digital space.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Inclusivity:
+            </span>{" "}
+            We are committed to building a team of experts, passionate and
+            dedicated to delivering exceptional digital experiences for our
+            clients. We strive to create a diverse and inclusive workforce that
+            values and supports the well-being of all our clients.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Professionalism:
+            </span>{" "}
+            We prioritize delivering high-quality, effective, and thoughtful
+            solutions that meet our clients' needs while ensuring they are
+            user-friendly and valuable.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Collaboration:
+            </span>{" "}
+            We value open communication and partnerships, working closely with
+            clients to ensure their vision and goals are at the heart of every
+            project.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Resilience:
+            </span>{" "}
+            We strive to create a supportive and resilient workforce that values
+            and supports the well-being of all our clients while delivering
+            user-friendly and valuable digital experiences.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Sustainability:
+            </span>{" "}
+            We prioritize delivering high-quality, effective, and thoughtful
+            solutions that meet our clients' needs, while also working to create
+            a sustainable and inclusive digital future for all.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">Trust:</span>{" "}
+            We are committed to building trust by delivering exceptional digital
+            experiences that are user-friendly, valuable, and aligned with our
+            clients' goals.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Excellence and Growth:
+            </span>{" "}
+            We hold ourselves to the highest standards, striving for excellence
+            in every detail while fostering continuous learning and innovation.
+          </li>
+          <li>
+            <span className="font-BonaNovaSC_Bold text-tertiary">
+              Community Impact:
+            </span>{" "}
+            We are passionate about using our expertise to make a positive
+            impact, helping businesses grow and contribute to their communities.
+          </li>
+        </ul>
+      </div>
+      <SidebarSeparator className="bg-secondary" />
+      <div className="py-3">
+        <h2>Our Team</h2>
+      </div>
     </main>
   );
 };
