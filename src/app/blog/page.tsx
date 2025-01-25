1281bc22145197a42b983b272dcdda5a0e2d1e76
Tina Huynh <tinamchuynh@gmail.com>
2025-01-24 22:37:29 -0800

    update filtering layout for blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,11 +2,12 @@
 
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/components/ui/sidebar";
+import { TopicFilterProps } from "@/lib/interfaces";
 import { blogs } from "@/lib/constants";
 import { cn } from "@/lib/utils";
 import {
+  Checkbox,
   FormControl,
-  InputLabel,
   MenuItem,
   OutlinedInput,
   Select,
@@ -20,9 +21,9 @@ const BlogDisplayPage: FC = () => {
   const { open } = useSidebar();
 
   const [filteredBlogs, setFilteredBlogs] = useState(blogs);
-  const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
-  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedDates, setSelectedDates] = useState<string[]>([]);
   const [noResults, setNoResults] = useState(false);
+  const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
   const [dropdownOpen, setDropdownOpen] = useState({
     topic: false,
     date: false,
@@ -38,12 +39,16 @@ const BlogDisplayPage: FC = () => {
       );
     }
 
-    if (selectedDate) {
-      filtered = filtered.filter((blog) => blog.date === selectedDate);
+    if (selectedDates.length > 0) {
+      filtered = filtered.filter((blog) => selectedDates.includes(blog.date));
     }
 
     if (filtered.length === 0) {
-      setNoResults(true);
+      if (filtersCleared) {
+        setTimeout(() => setNoResults(true), 2500);
+      } else {
+        setNoResults(true);
+      }
       setFilteredBlogs(blogs);
       setTimeout(() => setNoResults(false), 4000);
     } else {
@@ -54,7 +59,7 @@ const BlogDisplayPage: FC = () => {
 
   const clearFilters = () => {
     setSelectedTopics([]);
-    setSelectedDate("");
+    setSelectedDates([]);
     setNoResults(false);
     setFiltersCleared(true);
     setFilteredBlogs(blogs);
@@ -64,16 +69,13 @@ const BlogDisplayPage: FC = () => {
   const topics = Array.from(new Set(blogs.flatMap((blog) => blog.topics)));
   const dates = Array.from(new Set(blogs.map((blog) => blog.date)));
 
-  function handleTopicChange(event: SelectChangeEvent<typeof selectedTopics>) {
-    const {
-      target: { value },
-    } = event;
-    setSelectedTopics(typeof value === "string" ? value.split(",") : value);
-  }
+  const handleTopicChange = (updatedTopics: string[]) => {
+    setSelectedTopics(updatedTopics);
+  };
 
   useEffect(() => {
     handleFilter();
-  }, [selectedTopics, selectedDate]);
+  }, [selectedTopics, selectedDates]);
 
   function handleMouseLeave() {
     setDropdownOpen({
@@ -90,6 +92,24 @@ const BlogDisplayPage: FC = () => {
     });
   }
 
+  const handleTopicCheckboxChange = (topic: string, checked: boolean) => {
+    if (checked) {
+      handleTopicChange([...selectedTopics, topic]);
+    } else {
+      handleTopicChange(selectedTopics.filter((t) => t !== topic));
+    }
+    handleOpen("topic");
+  };
+
+  const handleDateCheckboxChange = (date: string, checked: boolean) => {
+    if (checked) {
+      setSelectedDates([...selectedDates, date]);
+    } else {
+      setSelectedDates(selectedDates.filter((d) => d !== date));
+    }
+    handleOpen("date");
+  };
+
   return (
     <main className="w-11/12 mx-auto py-6">
       <h1 className="text-3xl font-bold text-center">Our Latest Blogs</h1>
@@ -99,72 +119,79 @@ const BlogDisplayPage: FC = () => {
 
       {/* Filter Options */}
       <section className="my-8">
-        {/* Confirmation Text for Filters Cleared */}
-        {filtersCleared && (
-          <div className="my-4 text-center">
-            <p className="text-secondary">
-              Filters have been cleared successfully!
-            </p>
-          </div>
-        )}
+        <div className="h-40 md:h-32 lg:h-20 flex flex-col justify-between relative">
+          {/* Confirmation Text for Filters Cleared */}
+          {filtersCleared && (
+            <div className="mt-4 text-center h-14">
+              <p className="text-secondary m-0 p-0">
+                Filters have been cleared successfully!
+              </p>
+            </div>
+          )}
 
-        {/* No results warning */}
-        {noResults && (
-          <section className="my-8 text-center">
-            <p className="text-destructive">
-              No blogs match your selected filters. Filters have been cleared.
-            </p>
-          </section>
-        )}
+          {/* No results warning */}
+          {noResults && (
+            <div className="mt-4 text-center h-14">
+              <p className="text-destructive m-0">
+                No blogs match your selected filters. Filters have been cleared.
+              </p>
+            </div>
+          )}
+
+          {/* Clear Filters Button */}
+          <Button
+            variant={"destructive"}
+            className="my-4 absolute bottom-4"
+            onClick={clearFilters}
+          >
+            Clear Filters
+          </Button>
+        </div>
 
         <div
           className={cn(
-            "flex flex-col md:flex-row justify-center sm:items-end sm:space-y-4 md:gap-4",
+            "flex flex-col lg:flex-row justify-center sm:items-end sm:space-y-4 md:gap-4",
             {
-              "md:items-center lg:items-end lg:flex-row xl:flex-row": open,
+              "md:items-center lg:items-end lg:flex-row": open,
             }
           )}
         >
           <div
-            className={cn(
-              "flex flex-col md:flex-row justify-start space-y-4 sm:space-x-4 md:space-y-0 w-full pb-5",
-              {
-                "md:flex-col md:space-x-0 md:space-y-4 lg:flex-row lg:space-y-0 xl:flex-row":
-                  open,
-              }
-            )}
+            className={cn("w-full gap-3 grid grid-cols-1 pb-5 items-start", {
+              "md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-1": open,
+            })}
           >
             <div className="flex flex-wrap items-center">
               <label htmlFor="topic" className="mr-2 text-lg w-full">
                 Filter by Topic:
               </label>
               {/* Topic Filter */}
-              <div className="flex items-center w-full">
-                <FormControl className="w-11/12">
-                  <InputLabel id="demo-multiple-topic-label">
-                    Select Topics
-                  </InputLabel>
-                  <Select
-                    labelId="demo-multiple-topic-label"
-                    id="demo-multiple-topic"
-                    multiple
-                    value={selectedTopics}
-                    onChange={(e) => {
-                      handleTopicChange(e);
-                      handleOpen("topic");
-                    }}
-                    input={<OutlinedInput label="Select Topics" />}
-                    onMouseLeave={handleMouseLeave}
-                    onOpen={() => handleOpen("topic")}
-                    open={dropdownOpen.topic}
-                  >
-                    {topics.map((topic) => (
-                      <MenuItem key={topic} value={topic}>
-                        {topic}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+              <div
+                className={cn(
+                  "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-start items-start w-full",
+                  {
+                    "lg:grid-cols-1": open,
+                  }
+                )}
+              >
+                {topics.map((topic) => (
+                  <div key={topic} className="flex items-center mr-1">
+                    <Checkbox
+                      checked={selectedTopics.includes(topic)}
+                      onChange={(e) =>
+                        handleTopicCheckboxChange(topic, e.target.checked)
+                      }
+                    />
+                    <label
+                      htmlFor={topic}
+                      className="ml-2"
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleOpen("topic")}
+                    >
+                      {topic}
+                    </label>
+                  </div>
+                ))}
               </div>
             </div>
 
@@ -173,46 +200,35 @@ const BlogDisplayPage: FC = () => {
                 Filter by Date:
               </label>
               {/* Date Filter */}
-              <div className="flex items-center w-full">
-                <FormControl className="w-11/12">
-                  <InputLabel id="demo-date-select-label">
-                    Select Date
-                  </InputLabel>
-                  <Select
-                    labelId="demo-date-select-label"
-                    id="demo-date-select"
-                    value={selectedDate}
-                    onChange={(e) => {
-                      setSelectedDate(e.target.value);
-                      handleOpen("date");
-                    }}
-                    input={<OutlinedInput label="Select Date" />}
-                    onMouseLeave={handleMouseLeave}
-                    onOpen={() => handleOpen("date")}
-                    open={dropdownOpen.date}
-                  >
-                    <MenuItem value="">Select Date</MenuItem>
-                    {dates.map((date, index) => (
-                      <MenuItem key={index} value={date}>
-                        {date}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
+              <div
+                className={cn(
+                  "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-start items-start w-full",
+                  {
+                    "lg:grid-cols-1": open,
+                  }
+                )}
+              >
+                {dates.map((date, index) => (
+                  <div key={date} className="flex items-center mr-1">
+                    <Checkbox
+                      checked={selectedDates.includes(date)}
+                      onChange={(e) =>
+                        handleDateCheckboxChange(date, e.target.checked)
+                      }
+                    />
+                    <label
+                      htmlFor={date}
+                      className="ml-2"
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleOpen("date")}
+                    >
+                      {date}
+                    </label>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-
-          {/* Clear Filters Button */}
-          <Button
-            variant="link"
-            className={cn("sm:!mb-7", {
-              "md:!mb-0 lg:!mb-7": open,
-            })}
-            onClick={clearFilters}
-          >
-            Clear Filters
-          </Button>
         </div>
       </section>
 
@@ -232,17 +248,19 @@ const BlogDisplayPage: FC = () => {
           {filteredBlogs.map((blog, index) => (
             <div
               key={index}
-              className="border p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow"
+              className="border p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow flex flex-col justify-between"
             >
-              <h3 className="text-xl font-semibold mb-2">{blog.title}</h3>
-              <p className="text-gray-500 mb-4">
-                <span className="font-semibold">By {blog.author}</span> |{" "}
-                <span className="italic">{blog.date}</span>
-              </p>
-              <p className="text-lg mb-4">{blog.excerpt}</p>
-              <p className="text-sm text-gray-500 mb-4">
-                Topics: {blog.topics.join(", ")}
-              </p>
+              <div>
+                <h3 className="text-xl font-semibold mb-2">{blog.title}</h3>
+                <p className="mb-0">
+                  <span className="font-semibold">By {blog.author}</span>
+                </p>
+                <p>
+                  <span className="italic">{blog.date}</span>
+                </p>
+                <p className="text-lg mb-4">{blog.excerpt}</p>
+                <p className="text-sm mb-4">Topics: {blog.topics.join(", ")}</p>
+              </div>
               <Button
                 variant="link"
                 className="text-primary underline"
--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -16,3 +16,19 @@ export interface CollapsibleItemProps {
   isOpen: boolean;
   onToggle: () => void;
 }
+
+export interface TopicFilterProps {
+  topics: string[];
+  selectedTopics: string[];
+  handleTopicChange: ( topics: string[] ) => void;
+  handleOpen: ( filterType: string ) => void;
+  handleMouseLeave: () => void;
+}
+
+export interface DateFilterProps {
+  dates: string[];
+  selectedDates: string[];
+  setSelectedDate: ( dates: string[] ) => void;
+  handleOpen: ( filterType: string ) => void;
+  handleMouseLeave: () => void;
+}
