c824050ee3b11f4fe00fbf2af2c9df2d383679a1
Tina Huynh <tinamchuynh@gmail.com>
2025-01-26 10:48:59 -0800

    update website menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -88,18 +88,16 @@ const NavBar = () => {
                   <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                     <li className="row-span-3">
                       <NavigationMenuLink asChild>
-                        <a
-                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                          href="/"
-                        >
-                          <div className="mb-2 mt-4 text-lg font-medium">
-                            shadcn/ui
-                          </div>
-                          <p className="text-sm leading-tight text-muted-foreground">
-                            Beautifully designed components built with Radix UI
-                            and Tailwind CSS.
+                        <div className="flex h-full w-full select-none flex-col justify-end rounded-md p-6 no-underline outline-none focus:shadow-lg">
+                          <h2 className="text-lg font-medium mt-4 mb-2">
+                            Websites
+                          </h2>
+                          <p className="text-sm leading-tight">
+                            Explore expertly crafted solutions designed to meet
+                            your business needs. Beautiful, functional, and
+                            tailored just for you.
                           </p>
-                        </a>
+                        </div>
                       </NavigationMenuLink>
                     </li>
                     {websites.map((component) => (
