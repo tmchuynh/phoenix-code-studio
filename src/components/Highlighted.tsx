3c7d89d6f8bb5e13bb4ff9808cc3f0284528103f
Tina Huynh <39930092+tmchuynh@users.noreply.github.com>
2025-02-01 12:02:55 -0800

    Update Highlighted.tsx

--- src/components/Highlighted.tsx
+++ src/components/Highlighted.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const EmphasizeText = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <span className="underline underline-offset-4 text-primary">
-        {children}
-      </span>
+      <span className="text-primary">{children}</span>
     </>
   );
 };
