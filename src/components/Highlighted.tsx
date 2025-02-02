494aeaa87de65154bed6b7d69472c4787bb8f89a
Tina Huynh <tinamchuynh@gmail.com>
2025-02-01 20:00:12 -0800

    update text color for dark theme

--- src/components/Highlighted.tsx
+++ src/components/Highlighted.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const EmphasizeText = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <span className="text-primary">{children}</span>
+      <span className="text-primary dark:text-chart-1 dark:font-extrabold">
+        {children}
+      </span>
     </>
   );
 };
