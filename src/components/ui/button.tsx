613af78057e955c1392cdfbd7541c60df68813fb
Tina Huynh <tinamchuynh@gmail.com>
2025-02-02 11:43:34 -0800

    update text size to be more responsive

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-2 shadow-md font-extrabold",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-md lg:text-xl 2xl:text-2xl font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border-2 shadow-md dark:font-extrabold",
   {
     variants: {
       variant: {
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         accent:
           "bg-accent-4 text-accent-foreground hover:bg-transparent hover:border-accent-4 hover:text-accent-4 border-accent-4",
         destructive:
-          "bg-destructive font-bold text-destructive-foreground border-destructive hover:bg-transparent hover:text-foreground",
+          "bg-destructive text-destructive-foreground border-destructive hover:bg-transparent hover:text-foreground",
         outline: "bg-background hover:border-secondary hover:text-secondary",
         ghost:
           "shadow-none hover:bg-accent-1 hover:text-accent-foreground border-transparent",
