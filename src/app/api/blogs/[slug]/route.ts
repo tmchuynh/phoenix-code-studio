af81ca60a0ab89c4a4d5e9aaf5cfa580e96383a0
Tina Huynh <tinamchuynh@gmail.com>
2025-02-09 19:35:08 -0800

    update api route for blogs

--- src/app/api/blogs/[slug]/route.ts
+++ src/app/api/blogs/[slug]/route.ts
@@ -1,10 +1,15 @@
-// app/api/blog/[slug]/route.ts
-import { blogs } from "@/lib/blog-posts"; // Assuming your blog data is here
+import { blogs } from "@/lib/blog-posts";
 import { setSlug } from "@/lib/utils";
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET( _req: Request, { params }: { params: { slug: string; }; } ) {
-  const { slug } = params; // Get the dynamic slug parameter
+export async function GET( req: NextRequest ) {
+  // Extract slug from the pathname
+  const slug = req.nextUrl.pathname.split( "/" ).pop(); // Get the last part of the URL
+
+  if ( !slug ) {
+    return NextResponse.json( { message: "Blog post not found" }, { status: 404 } );
+  }
 
   // Generate the blog slug dynamically based on the title (if needed)
   const blogPost = blogs.find( ( blog ) => setSlug( blog.title ) === slug ); // Use setSlug to find the post
