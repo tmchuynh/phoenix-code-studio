2711ae24283289257b405966d4b8050099ceecfe
Tina Huynh <tinamchuynh@gmail.com>
2025-02-15 09:35:15 -0800

    update checkbox colors and gradients

--- src/components/ui/checkbox-custom.tsx
+++ src/components/ui/checkbox-custom.tsx
@@ -8,15 +8,17 @@ const BpIcon = styled("span")(({ theme }) => ({
   height: 16,
   boxShadow:
     "inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)",
-  backgroundColor: "#f5f8fa",
+  backgroundColor: "#415A77",
   backgroundImage:
-    "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
+    "linear-gradient(160deg,hsla(50,0%,100%,.99),hsla(0,0%,90%,.3))",
   ".Mui-focusVisible &": {
     outline: "2px auto rgba(19,124,189,.6)",
     outlineOffset: 2,
   },
   "input:hover ~ &": {
-    backgroundColor: "#ebf1f5",
+    backgroundColor: "#A3B6CC",
+    backgroundImage:
+      "linear-gradient(160deg,hsla(50,0%,100%,.99),hsla(0,0%,90%,.3))",
     ...theme.applyStyles("dark", {
       backgroundColor: "#30404d",
     }),
@@ -37,9 +39,9 @@ const BpIcon = styled("span")(({ theme }) => ({
 }));
 
 const BpCheckedIcon = styled(BpIcon)({
-  backgroundColor: "#E05260",
+  backgroundColor: "#83C5BE",
   backgroundImage:
-    "linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))",
+    "linear-gradient(160deg,hsla(50,0%,100%,.89),hsla(0,0%,90%,.3))",
   "&::before": {
     display: "block",
     width: 16,
@@ -51,7 +53,9 @@ const BpCheckedIcon = styled(BpIcon)({
     content: '""',
   },
   "input:hover ~ &": {
-    backgroundColor: "#FF4B0A",
+    backgroundColor: "#02c39a",
+    backgroundImage:
+      "linear-gradient(160deg,hsla(50,0%,100%,.89),hsla(0,0%,90%,.3))",
   },
 });
 
