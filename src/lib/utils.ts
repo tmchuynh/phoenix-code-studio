fbd74bf8d2b0ba92855fbbf6297755282ddc132f
Tina Huynh <tinamchuynh@gmail.com>
2025-02-15 20:34:20 -0800

    fix slug formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,53 +1,52 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn( ...inputs: ClassValue[] ) {
-  return twMerge( clsx( inputs ) );
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
 
-export const formatCurrency = ( value: number ) => {
-  if ( isNaN( value ) ) return "$0.00";
+export const formatCurrency = (value: number) => {
+  if (isNaN(value)) return "$0.00";
 
-  return "$" + value.toLocaleString( undefined );
+  return "$" + value.toLocaleString(undefined);
 };
 
-export const capitalize = ( str: string ) => {
-  return str.replace( /-/g, " " ).replace( /\b\w/g, ( char ) => char.toUpperCase() );
+export const capitalize = (str: string) => {
+  return str.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
 };
 
-export const formatQuotes = ( quote: string ): string[] => {
+export const formatQuotes = (quote: string): string[] => {
   return quote
-    .split( "\n" )
-    .map( ( paragraph ) => paragraph.trim() )
-    .filter( ( paragraph ) => paragraph !== "" );
+    .split("\n")
+    .map((paragraph) => paragraph.trim())
+    .filter((paragraph) => paragraph !== "");
 };
 
-export const formatQuote = ( quote: string ): string[] => {
+export const formatQuote = (quote: string): string[] => {
   return quote
-    .split( "\n" )
-    .map( ( paragraph ) => paragraph.trim() )
-    .filter( ( paragraph ) => paragraph !== "" );
+    .split("\n")
+    .map((paragraph) => paragraph.trim())
+    .filter((paragraph) => paragraph !== "");
 };
 
-export const formatDate = ( dateString: string ): string => {
-  const date = new Date( dateString );
+export const formatDate = (dateString: string): string => {
+  const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = ( date.getMonth() + 1 ).toString().padStart( 2, "0" );
-  const day = date.getDate().toString().padStart( 2, "0" );
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
-  return `${ year }-${ month }-${ day }`;
+  return `${year}-${month}-${day}`;
 };
 
-export function setSlug( title: string ): string {
+export function setSlug(title: string): string {
   const slug = title
     .toLowerCase()
-    .replace( /[!@#$?%,/^&*]/g, "" )
-    .replace( /[\s-]+/g, "-" )
-    .replace( / /g, "-" );
+    .replace(/[!@#$?%,/^&*]/g, "")
+    .replace(/[\s-]+/g, "-");
   return slug;
 }
 
-export function formatName( name: string ): string {
-  const formattedName = name.replace( /-/g, " " );
-  return capitalize( formattedName );
+export function formatName(name: string): string {
+  const formattedName = name.replace(/-/g, " ");
+  return capitalize(formattedName);
 }
