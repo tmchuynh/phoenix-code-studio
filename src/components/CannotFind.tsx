52252857a98abd1801eafe010cccb582cee1dbfa
Tina Huynh <tinamchuynh@gmail.com>
2025-02-17 15:21:22 -0800

    recenter component on page

--- src/components/CannotFind.tsx
+++ src/components/CannotFind.tsx
@@ -3,7 +3,7 @@ import { SpinningText } from "./magicui/spinning-text";
 
 const CannotFind: React.FC = () => {
   return (
-    <main className="w-10/12 md:w-11/12 mx-auto py-6 h-lvh flex justify-center items-center">
+    <main className="w-10/12 md:w-11/12 mx-auto -mt-28 h-svh flex justify-center items-center">
       <SpinningText reverse className="text-4xl" duration={4} radius={6}>
         the data on this page • cannot be loaded • at this time •
       </SpinningText>
