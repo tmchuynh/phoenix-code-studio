d27692fb8b26f5cf82eec62d13de11a173f473b1
Tina Huynh <tinamchuynh@gmail.com>
2025-02-17 15:22:41 -0800

    start to design dynamic pages for contracts

---
+++ src/app/services/contracts/[category]/[contract]/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import CannotFind from "@/components/CannotFind";
+import LoadingIndicator from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import { Category, SubItem } from "@/lib/interfaces";
+import useMediumScreen from "@/lib/useMediumScreen";
+import useSmallScreen from "@/lib/useSmallScreen";
+import { formatName, setSlug } from "@/lib/utils";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function CategoryPage() {
+  const { category, contract } = useParams() as {
+    category: string;
+    contract: string;
+  };
+
+  const isMediumScreen = useMediumScreen();
+  const isSmallScreen = useSmallScreen();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [contractDetail, setContractDetail] = useState<SubItem | null>(null);
+
+  useEffect(() => {
+    if (!category || !contract) return;
+
+    async function fetchServices() {
+      try {
+        const response = await fetch(
+          `/api/services/contracts/${category}/${contract}`
+        );
+        if (!response.ok) {
+          throw new Error("Services post not found");
+        }
+        const data: SubItem = await response.json();
+        setContractDetail(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : String(err));
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+      }
+    }
+
+    fetchServices();
+  }, [category]);
+
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) return <CannotFind />;
+
+  return (
+    <main className="w-10/12 md:w-11/12 mx-auto py-6">
+      <h1>
+        {contractDetail?.info.name && formatName(contractDetail?.info.name)}
+      </h1>
+
+      <p className="mb-4">
+        {isSmallScreen
+          ? contractDetail?.info.short
+          : isMediumScreen
+          ? contractDetail?.info.details
+          : contractDetail?.info.info}
+      </p>
+
+      {contractDetail?.details && (
+        <div className="ml-5 md:ml-9 lg:ml-10">
+          <ol className="list-roman space-y-3">
+            {contractDetail?.details?.map((detail, detailIndex) => (
+              <li key={detailIndex}>
+                <h3>{detail.title}</h3>
+                <p>{detail.description}</p>
+              </li>
+            ))}
+          </ol>
+        </div>
+      )}
+
+      <div className="pt-10">
+        <h2>{contractDetail?.cta.title}</h2>
+        <p>{contractDetail?.cta.intro && contractDetail?.cta.intro}</p>
+        <p>
+          {contractDetail?.cta.text && contractDetail?.cta.text}{" "}
+          {contractDetail?.cta.afterButtonText &&
+            contractDetail?.cta.afterButtonText}
+        </p>
+
+        <Button className="my-3" onClick={() => router.push("/contact-us")}>
+          {contractDetail?.cta.button}
+        </Button>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/services/contracts/[category]/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import CannotFind from "@/components/CannotFind";
+import LoadingIndicator from "@/components/Loading";
+import { Button } from "@/components/ui/button";
+import { Category } from "@/lib/interfaces";
+import useMediumScreen from "@/lib/useMediumScreen";
+import useSmallScreen from "@/lib/useSmallScreen";
+import { formatName, setSlug } from "@/lib/utils";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { LuArrowBigRightDash } from "react-icons/lu";
+
+export default function CategoryPage() {
+  const { category } = useParams() as { category: string };
+  const isMediumScreen = useMediumScreen();
+  const isSmallScreen = useSmallScreen();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [contract, setContract] = useState<Category | null>(null);
+
+  useEffect(() => {
+    if (!category) return;
+
+    async function fetchServices() {
+      try {
+        const response = await fetch(`/api/services/contracts/${category}`);
+        if (!response.ok) {
+          throw new Error("Services post not found");
+        }
+        const data = await response.json();
+        setContract(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : String(err));
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+      }
+    }
+
+    fetchServices();
+  }, [category]);
+
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) return <CannotFind />;
+
+  const navigateToDetails = (serviceName: string) => {
+    const formattedServiceName = setSlug(serviceName);
+
+    router.push(`/services/${category}/${formattedServiceName}`);
+  };
+
+  return (
+    <main className="w-10/12 md:w-11/12 mx-auto py-6">
+      <h1>{contract?.name && formatName(contract?.name)}</h1>
+      <div className="mb-4">
+        {" "}
+        {isSmallScreen
+          ? contract?.info.short
+          : isMediumScreen
+          ? contract?.info.detail
+          : contract?.info.description.map((sentence, index) => {
+              return <p key={index}>{sentence}</p>;
+            })}
+      </div>
+
+      <h2>Discover What We Can Do For You</h2>
+      <p>{contract?.info.intro}</p>
+
+      {contract?.info?.sub?.length ? (
+        contract.info?.sub.map((sub, index) => (
+          <div className="py-1 flex" key={index}>
+            <p className="group flex items-center gap-5">
+              {sub
+                .split("-")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}{" "}
+              <span className="inline-block transition-transform duration-300 ease-in-out group-hover:translate-x-5 text-accent-2">
+                <LuArrowBigRightDash />
+              </span>
+              <Button
+                variant={"link"}
+                className="no-underline hover:underline"
+                onClick={() => navigateToDetails(sub)}
+              >
+                Learn More
+              </Button>
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>No sub-services found.</p>
+      )}
+    </main>
+  );
+}
