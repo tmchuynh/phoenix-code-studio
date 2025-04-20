9df1f3fa03ed9baa546ebeb866463d43b5b515ab
Tina Huynh <tinamchuynh@gmail.com>
2025-04-19 19:09:28 -0700

    separate constants

---
+++ src/lib/constants/navigation.ts
@@ -0,0 +1,61 @@
+import { Menu } from "../types";
+
+export const about: Menu[] = [
+  {
+    title: "About Us",
+    href: "/about-us",
+    description:
+      "Discover our mission, values, and the story behind our brand.",
+  },
+  {
+    title: "Careers",
+    href: "",
+    description:
+      "Join our team and explore exciting career opportunities with us.",
+  },
+  {
+    title: "Blog",
+    href: "/blogs",
+    description:
+      "Stay informed with expert insights, industry trends, and company updates.",
+  },
+  {
+    title: "FAQs",
+    href: "/services/frequently-asked-questions",
+    description:
+      "Get answers to commonly asked questions about our services and processes.",
+  },
+  {
+    title: "Testimonials",
+    href: "/about-us/testimonials",
+    description:
+      "See what our clients say about their experiences working with us.",
+  },
+];
+
+export const serviceCategories: Menu[] = [
+  {
+    title: "Overview",
+    href: "/services",
+    description:
+      "Explore our range of flexible payment plans designed to fit your needs and budget seamlessly.",
+  },
+  {
+    title: "Enterprise-Grade Digital Solutions",
+    href: "/services/scalable-corporate-digital-solutions",
+    description:
+      "Unlock powerful digital solutions tailored for corporations looking to scale and streamline operations.",
+  },
+  {
+    title: "SEO-Driven Content Strategy",
+    href: "/services/seo-optimized-content-creation",
+    description:
+      "Boost your online visibility with expertly crafted, search-optimized content that drives engagement.",
+  },
+  {
+    title: "End-to-End Website Development",
+    href: "/services/comprehensive-website-solutions",
+    description:
+      "From design to deployment, build and optimize a high-performing website tailored to your goals.",
+  },
+];
